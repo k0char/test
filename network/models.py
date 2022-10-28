09129c05f94e0143d6843a900d14b1549d332134
K0char <michalkucharewicz0@gmail.com>
2022-10-28 15:24:34 +0200

    init+

---
+++ network/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+import datetime
+import django.utils.timezone
+
+class User:
+    ...
+class Post:
+    ...
---
+++ network/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("create", views.newpost, name="create"),
+    path("all_posts", views.all_posts, name="all_posts")
+]
